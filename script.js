73f1023fd35696bbd055979f4eaaebfd01bcc685
Duvan <ayaladuvan876@gmail.com>
2022-06-26 18:24:39 -0400

    List of all caracters available

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword() {
 
-  passwordText.value = password;
+  // list of all characters, numbers & symbols available to generate password
+    var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var symbol = ["!", "@", "$", "%", "&", "£", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "#", "^", "_", "~", "|"];
+    var userChoice = [];
 
+// ask user for their desired password length
+    var characterPassword = window.prompt("How many characters would you like your password to be?");
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    generatePassword();
